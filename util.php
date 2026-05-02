ff63509b2dd33fae82b2e7e0d0645760d633a3c5
Biniam Alemayehu <bini@Biniams-MacBook-Pro-Main-64GB.local>
2026-05-02 09:39:51 -0400

    updated api to return specific data

--- util.php
+++ util.php
@@ -381,9 +381,10 @@ function handleSqlActionRequest(string $sqlDirectory, string $databaseName): voi
 
 		$dataset = fetchHairSalonDataset($connection, $sqlDirectory);
 		if ($dataSelector !== "") {
-			$dataset = [
-				$dataSelector => $dataset[$dataSelector] ?? [],
-			];
+			header("Content-Type: application/json; charset=utf-8");
+			http_response_code(200);
+			echo json_encode($dataset[$dataSelector] ?? [], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+			exit;
 		}
 
 		respondSqlActionJson([
